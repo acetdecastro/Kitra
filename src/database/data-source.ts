7d03036dda84573f2a1529e78f9e565d1b33d6b2
Ace De Castro <acetdecastro@gmail.com>
2024-03-15 03:00:10 +0800

    Chore: Update entities path

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -2,9 +2,6 @@ import { envConfig } from '../config';
 import { DataSource, DataSourceOptions } from 'typeorm';
 import { SeederOptions } from 'typeorm-extension';
 
-// Define the base directory for entities
-const entitiesBaseDir = __dirname + '/../';
-
 const options: DataSourceOptions & SeederOptions = {
   type: 'mysql',
   host: envConfig.DB_HOST,
@@ -15,7 +12,7 @@ const options: DataSourceOptions & SeederOptions = {
   synchronize: true,
   migrationsTableName: 'migrations',
   logging: false,
-  entities: [entitiesBaseDir + '**/*.entity.ts'],
+  entities: [__dirname + '/../**/*.entity.ts'],
   subscribers: [],
   migrations: [],
   seeds: ['src/database/seeds/**/*{.ts,.js}'],
