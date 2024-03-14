d1e9efc800dc0482af8b63db34cc5a053abcc243
Ace De Castro <acetdecastro@gmail.com>
2024-03-15 04:17:41 +0800

    Chore: Add Treasure and MoneyValue entities (#3)
    * Chore: Add interfaces
    
    * Chore: Add Treasure and MoneyValue entities
    
    * Chore: Add Treasure and MoneyValue seeder

--- src/database/seeds/initial-data.seeder.ts
+++ src/database/seeds/initial-data.seeder.ts
@@ -1,7 +1,16 @@
 import { Seeder } from 'typeorm-extension';
-import { ObjectLiteral, Repository } from 'typeorm';
+import { DeepPartial, Repository } from 'typeorm';
 import { User } from '../../users/user.entity';
-import { users } from '../seed-data';
+import { Treasure } from '../../treasures/treasure.entity';
+import { MoneyValue } from '../../treasures/money-value.entity';
+import {
+  users,
+  treasures,
+  moneyValues,
+  IUser,
+  ITreasure,
+  IMoneyValue,
+} from '../seed-data';
 import { AppDataSource } from '../data-source';
 import bcrypt from 'bcrypt';
 
@@ -11,7 +20,15 @@ export default class InitialDataSeeder implements Seeder {
       await AppDataSource.initialize();
       console.log(`Successfully connected with MySQL`);
 
-      await this.seedEntities(AppDataSource.getRepository(User), users);
+      await this.seedUsers(AppDataSource.getRepository(User), users);
+      await this.seedTreasures(
+        AppDataSource.getRepository(Treasure),
+        treasures,
+      );
+      await this.seedMoneyValues(
+        AppDataSource.getRepository(MoneyValue),
+        moneyValues,
+      );
 
       console.log('Seeding complete');
     } catch (error) {
@@ -19,21 +36,49 @@ export default class InitialDataSeeder implements Seeder {
     }
   }
 
-  private async seedEntities<T extends ObjectLiteral>(
-    repository: Repository<T>,
-    data: T[],
+  private async seedUsers(
+    repository: Repository<User>,
+    userData: IUser[],
   ): Promise<void> {
     await Promise.all(
-      data.map(async (entity) => {
-        if ('password' in entity && entity.password) {
-          const user = User.create(entity);
-          const salt = await bcrypt.genSalt();
-          user.password = await bcrypt.hash(user.password, salt);
-          Object.assign(entity, user);
+      userData.map(async (user) => {
+        const newUser = User.create(user as DeepPartial<User>);
+        const salt = await bcrypt.genSalt();
+        newUser.password = await bcrypt.hash(newUser.password, salt);
+        await repository.save(newUser);
+      }),
+    );
+  }
+
+  private async seedTreasures(
+    repository: Repository<Treasure>,
+    treasureData: ITreasure[],
+  ): Promise<void> {
+    await Promise.all(
+      treasureData.map(async (treasure) => {
+        const newTreasure = Treasure.create(treasure as DeepPartial<User>);
+        await repository.save(newTreasure);
+      }),
+    );
+  }
+
+  private async seedMoneyValues(
+    repository: Repository<MoneyValue>,
+    moneyValuesData: IMoneyValue[],
+  ): Promise<void> {
+    await Promise.all(
+      moneyValuesData.map(async (moneyValueData) => {
+        const treasureId = moneyValueData.treasure_id;
+        const treasure = await repository.manager.findOne(Treasure, {
+          where: { id: treasureId },
+        });
+        if (treasure) {
+          const moneyValue = new MoneyValue();
+          moneyValue.amt = moneyValueData.amt;
+          moneyValue.treasure = treasure;
+          await repository.save(moneyValue);
         }
       }),
     );
-
-    await repository.save(data);
   }
 }
