3df627a45ba073e0c0d842a75eac113265e08529
Ace De Castro <acetdecastro@gmail.com>
2024-03-15 00:30:02 +0800

    Chore: Add database connection (#1)

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,6 +4,11 @@ import z from "zod";
 const envSchema = z.object({
   PORT: z.coerce.number().min(1000),
   NODE_ENV: z.enum(["development", "production", "test"]),
+  DB_HOST: z.string(),
+  DB_PORT: z.coerce.number().min(1000),
+  DB_USERNAME: z.string(),
+  DB_PASSWORD: z.string(),
+  DB_NAME: z.string(),
 });
 
 const envServer = envSchema.safeParse(process.env);
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
 import express from "express";
 import bodyParser from "body-parser";
 import { envConfig } from "./config";
+import { AppDataSource } from "./database";
 
 const app = express();
 
 app.use(bodyParser.json());
+
+AppDataSource.initialize()
+  .then(() => {
+    console.log(`Successfully connected with MySQL`);
+  })
+  .catch((error) => console.log(error));
 
 app.listen(envConfig.PORT, () => {
   console.log(`Server is listening on port: ${envConfig.PORT}`);
